811eb6cd1ffe97dc396aee39c96911d119b87ada
anandg112 <anandg112@gmail.com>
2017-02-10 17:08:27 +0000

    private function module export

---
+++ calc.js
@@ -0,0 +1,5 @@
+var math2 = require('./math2.js');
+
+console.log(math2.PI);
+
+math2.val();
---
+++ math.js
@@ -0,0 +1,11 @@
+function sqrt(num){
+  return Math.sqrt(num);
+}
+
+
+module.exports = {
+  PI: Math.PI,
+  val: function() {
+    console.log(sqrt(2));
+  }
+};
